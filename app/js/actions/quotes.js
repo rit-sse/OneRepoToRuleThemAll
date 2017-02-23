b4185ecee69627eb3c49b93e2179ef382c0dd816
Dylan Vuz <vuzdylan@gmail.com>
2017-02-23 17:07:21 -0500

    polly fill plus scrolling

---
+++ app/js/actions/quotes.js
@@ -0,0 +1,23 @@
+import * as utils from './utils';
+
+export const QUOTES = 'QUOTES';
+export const GET_QUOTES = 'GET_QUOTES';
+export const GET_QUOTE_PAGE = 'GET_QUOTE_PAGE';
+export const APPROVE_QUOTE = 'APPROVE_QUOTE';
+export const CREATE_QUOTE = 'CREATE_QUOTE';
+export const UPDATE_QUOTE = 'UPDATE_QUOTE';
+export const DESTROY_QUOTE = 'DESTROY_QUOTE';
+
+const createAction = utils.createAction(QUOTES);
+const loading = utils.createLoading(QUOTES);
+
+export function getQuotes(getNext, tag, search, approved = true) {
+  return (dispatch, getState, api) => {
+    if (getNext) {
+      const page = getState().quotes.pagination.currentPage + 1;
+      api.Quotes.all({ page, tag, approved, search })
+        .then(({ data }) => dispatch(createAction(GET_QUOTE_PAGE, data)))
+        .catch(err => dispatch(createAction(GET_QUOTE_PAGE, err)));
+    }
+  };
+}
