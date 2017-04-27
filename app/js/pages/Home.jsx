336d36866f79982a7b72876f44e41d9d20a89be2
Dylan Vuz <vuzdylan@gmail.com>
2017-04-27 13:42:02 -0400

    work on front page

--- app/js/pages/Home.jsx
+++ app/js/pages/Home.jsx
@@ -1,37 +1,42 @@
 import React from 'react';
 import HomeEvents from 'containers/events/HomeEvents';
-import frontPage from 'img/front-page.png';
+import frontPage from 'img/lab.png';
 import 'scss/home.scss';
 
 const PageHome = () => (
   <div>
     <div className="row bot-1">
       <div className="col-12 hero">
-        <img className="image" src={frontPage} alt="of lab" />
-        <div className="pillars">
-          <p className="pillar">Mentoring</p>
-          <p className="pillar">Networking</p>
-          <p className="pillar">Projects</p>
-          <p className="pillar">Events</p>
+        <div className="container">
+          <div className="pillars">
+            <p className="pillar">Mentoring</p>
+            <p className="pillar">Networking</p>
+            <p className="pillar">Projects</p>
+            <p className="pillar">Events</p>
+          </div>
+          <img className="image" src={frontPage} alt="of lab" />
         </div>
       </div>
     </div>
     <div className="row">
-      <div className="col-7 offset-md-1 col-md-6">
+      <div className="col-12 offset-md-1 col-md-6">
         <div className="col-12">
-          <h4>Mentoring:</h4>
-          <p>The SSE has a mentor on duty every day that can help with intro SE and CS classes</p>
-          <p>Hours: 10am - 6pm Mon - Fri</p>
+          <h4>Mentoring</h4>
+          <p>
+            The SSE has a mentor on duty every day that can help with intro SE and CS classes.
+            There is also a physics tutor who hold open mentoring hours in our lab.
+            Mentoring hours are 10am - 6pm Mon - Fri
+          </p>
         </div>
         <div className="col-12">
-          <h4>Projects:</h4>
-          <p><strong style={{ color: '#1E5A8D' }}>Sandbox:</strong> SSE plays with sand</p>
-          <p><strong style={{ color: '#1E5A8D' }}>HDR Display:</strong> SSE breaks a TV</p>
-          <p><strong style={{ color: '#1E5A8D' }}>Jukebox</strong> SSE relives the 80&apos;s</p>
-          <p><strong style={{ color: '#1E5A8D' }}>Course Planner</strong> SSE copies CSH</p>
+          <h4>Getting involved</h4>
+          <p>
+            Want to get involved in the SSE? Feel free to stop by our mentoring lab at RIT.
+            You can find us in building 70 (Golisano), room 1670. Students of any major are welcome!
+          </p>
         </div>
       </div>
-      <div className="col-5 col-md-4 left-border">
+      <div className="col-12 col-md-4 left-border">
         <div className="col-12">
           <h4>Upcoming Events</h4>
           <HomeEvents />
