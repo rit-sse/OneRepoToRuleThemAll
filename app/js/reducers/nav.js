f702063455c9152804657f7830117e8b6f66b41c
Dylan Vuz <vuzdylan@gmail.com>
2017-05-02 18:30:50 -0400

    fixed nav not closing on route change

--- app/js/reducers/nav.js
+++ app/js/reducers/nav.js
@@ -1,10 +1,12 @@
 import { OPEN_NAV, CLOSE_NAV } from 'actions/nav';
+import { LOCATION_CHANGE } from 'connected-react-router';
 
 export default function nav(state = false, action) {
   switch (action.type) {
     case OPEN_NAV:
       return true;
     case CLOSE_NAV:
+    case LOCATION_CHANGE:
       return false;
     default:
       return state;
