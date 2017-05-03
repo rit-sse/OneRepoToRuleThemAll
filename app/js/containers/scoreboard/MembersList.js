f1378555f884471ee28077ac20c930d156c9f50f
Dylan Vuz <vuzdylan@gmail.com>
2017-05-03 11:46:24 -0400

    Fixes scoreboard not sorting memberships

--- app/js/containers/scoreboard/MembersList.js
+++ app/js/containers/scoreboard/MembersList.js
@@ -8,7 +8,7 @@ function mapStateToProps(store) {
   return {
     item: Member,
     items: Object.keys(store.members).sort((a, b) => {
-      return a.count - b.count;
+      return store.members[a].count - store.members[b].count;
     }).map((el, index) => {
       return {
         ...store.members[el],
