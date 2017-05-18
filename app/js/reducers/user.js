8a63a8f247f9dc5f9a7fbe6818157cf4a655331d
Dylan Vuz <vuzdylan@gmail.com>
2017-05-18 15:16:56 -0400

    fixes #32

--- app/js/reducers/user.js
+++ app/js/reducers/user.js
@@ -1,11 +1,16 @@
 import {
   GET_USER,
 } from 'actions/user';
+import {
+  HIDE_MODAL,
+} from 'actions/modal';
 
 export default function user(state = {}, action) {
   switch (action.type) {
     case GET_USER:
       return action.payload;
+    case HIDE_MODAL:
+      return {};
     default:
       return state;
   }
