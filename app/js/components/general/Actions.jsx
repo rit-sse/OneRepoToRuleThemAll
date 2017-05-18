31ff7cc89a64696a00f5c859f935c49881b4b785
Dylan Vuz <vuzdylan@gmail.com>
2017-05-18 14:35:29 -0400

    fixes #22

---
+++ app/js/components/general/Actions.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const Actions = ({
+  show,
+  shown,
+  children,
+  editItem,
+  deleteItem,
+}) => (
+  show ? (
+    <div className={shown ? 'actions shown' : 'actions'}>
+      {children}
+      <button className="btn btn-small btn-info" onClick={editItem}>
+        <i className="fa fa-pencil" aria-hidden="true" />
+      </button>
+      <button className="btn btn-small btn-danger" onClick={deleteItem}>
+        <i className="fa fa-trash-o" aria-hidden="true" />
+      </button>
+    </div>
+  ) : null
+);
+
+Actions.propTypes = {
+  show: PropTypes.bool.isRequired,
+  children: PropTypes.node,
+  shown: PropTypes.bool.isRequired,
+  editItem: PropTypes.func.isRequired,
+  deleteItem: PropTypes.func.isRequired,
+};
+
+Actions.defaultProps = {
+  children: [],
+};
+
+export default Actions;
--- app/js/components/qdb/PendingQuote.jsx
+++ app/js/components/qdb/PendingQuote.jsx
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import Tags from 'components/qdb/Tags';
 import SwipeArea from 'components/general/SwipeArea';
+import Actions from 'components/general/Actions';
 
 import 'scss/pane.scss';
+import 'scss/actions.scss';
 
 class PendingQuote extends Component {
   static propTypes = {
@@ -59,16 +61,19 @@ class PendingQuote extends Component {
     ));
 
     return (
-      <SwipeArea onLeft={this.showActions} onRight={this.hideActions} className="pane quote">
+      <SwipeArea onLeft={this.showActions} onRight={this.hideActions} className="pane quote actions-container">
         <div className="heading">
           <blockquote>{bodyText}</blockquote>
-          { loggedIn ? (
-            <div className={this.state.shown ? 'actions shown' : 'actions'}>
-              <button className="btn btn-small btn-success" onClick={() => approveQuote(id)}><i className="fa fa-rocket" aria-hidden="true" /> Approve</button>
-              <button className="btn btn-small btn-info" onClick={editItem}><i className="fa fa-pencil" aria-hidden="true" /> Edit</button>
-              <button className="btn btn-small btn-danger" onClick={deleteItem}><i className="fa fa-trash-o" aria-hidden="true" /> Delete</button>
-            </div>
-          ) : null }
+          <Actions
+            show={loggedIn}
+            shown={this.state.shown}
+            editItem={editItem}
+            deleteItem={deleteItem}
+          >
+            <button className="btn btn-small btn-success" onClick={() => approveQuote(id)}>
+              <i className="fa fa-rocket" aria-hidden="true" />
+            </button>
+          </Actions>
         </div>
         <small className="description">{description}</small>
         <Tags tags={tags} link="approval" />
