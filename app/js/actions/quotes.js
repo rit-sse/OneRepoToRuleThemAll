bf094ad3a415cddc85e9a4e322e7d278e19d7a74
Dylan Vuz <vuzdylan@gmail.com>
2017-06-08 22:11:00 -0700

    fix qdb and auth

--- app/js/actions/quotes.js
+++ app/js/actions/quotes.js
@@ -1,8 +1,8 @@
+import { scrollDone } from 'actions/scroll';
 import * as utils from './utils';
 
 export const QUOTES = 'QUOTES';
 export const GET_QUOTES = 'GET_QUOTES';
-export const GET_QUOTE_PAGE = 'GET_QUOTE_PAGE';
 export const APPROVE_QUOTE = 'APPROVE_QUOTE';
 export const CREATE_QUOTE = 'CREATE_QUOTE';
 export const UPDATE_QUOTE = 'UPDATE_QUOTE';
@@ -13,22 +13,18 @@ const loading = utils.createLoading(QUOTES);
 
 export function getQuotes(getNext, tag, search, approved = true) {
   return (dispatch, getState, { Quotes }) => {
-    if (getNext) {
-      if (getState().status.loading[GET_QUOTES] || getState().status.loading[GET_QUOTE_PAGE]) return;
-      dispatch(loading(GET_QUOTE_PAGE));
-      const page = getState().quotes.pagination.currentPage + 1;
-      Quotes.all({ page, tag, approved, search })
-        .then(data => dispatch(createAction(GET_QUOTE_PAGE, data)))
-        .catch(err => dispatch(createAction(GET_QUOTE_PAGE, err)));
-    } else {
-      dispatch(loading(GET_QUOTES));
-      Quotes.all({
-        tag,
-        approved,
-        search,
-      }).then(data => dispatch(createAction(GET_QUOTES, data)))
-        .catch(err => dispatch(createAction(GET_QUOTES, err)));
-    }
+    if (getState().status.loading[GET_QUOTES]) return;
+    dispatch(loading(GET_QUOTES));
+    Quotes.next({ tag, approved, search })
+      .then((data) => {
+        if (data.length > 0) {
+          dispatch(createAction(GET_QUOTES, { quotes: data, paged: getNext }));
+        } else {
+          dispatch(scrollDone());
+          dispatch(createAction(GET_QUOTES, { quotes: [], paged: getNext }));
+        }
+      })
+      .catch(err => dispatch(createAction(GET_QUOTES, err)));
   };
 }
 
