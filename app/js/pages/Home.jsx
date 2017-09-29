f23a2ac108ac1e79aeeaec0177ee33092ef4e5b0
Wade Mauger <wadeanthony0100@gmail.com>
2017-09-29 15:34:21 -0400

    Update verbiage

--- app/js/pages/Home.jsx
+++ app/js/pages/Home.jsx
@@ -17,8 +17,8 @@ const PageHome = () => (
       <div className="flex-2 flex-content">
         <h4 className="front-heading">Mentoring <small>10am - 6pm, Mon - Fri</small></h4>
         <p>
-          The SSE has a mentor on duty every day that can help with SE, CS and other technical classes.
-          There is also a physics tutor who holds open mentoring hours in our lab.
+          The SSE has mentors on duty every day who can help with SE, CS and other technical classes.
+          There are also physics tutors who hold open mentoring hours in our lab.
         </p>
         <h4 className="front-heading">Getting involved</h4>
         <p>
