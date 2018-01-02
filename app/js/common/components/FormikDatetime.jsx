feba9153785dcefc17182ecc6b3afeec23e125d2
Bill Dybas <wmdybas@gmail.com>
2018-01-01 19:26:47 -0500

    Add Datetime Component Compatible w/ Formik

---
+++ app/js/common/components/FormikDatetime.jsx
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import Datetime from 'react-datetime';
+
+// See: https://github.com/jaredpalmer/formik#why-use-setfieldvalue-instead-of-handlechange
+class FormikDatetime extends Component {
+  static propTypes = {
+    inputProps: PropTypes.object, // eslint-disable-line react/forbid-prop-types
+    value: PropTypes.any.isRequired, // eslint-disable-line react/forbid-prop-types
+    field: PropTypes.string.isRequired,
+    onChange: PropTypes.func.isRequired, // setFieldValue
+    onBlur: PropTypes.func.isRequired, // setFieldTouched
+  };
+
+  static defaultProps = {
+    inputProps: {},
+  };
+
+  handleChange = (value) => {
+    this.props.onChange(this.props.field, value);
+  };
+
+  handleBlur = () => {
+    this.props.onBlur(this.props.field, true);
+  };
+
+  render() {
+    const {
+      inputProps,
+      value,
+    } = this.props;
+
+    return (
+      <Datetime
+        inputProps={inputProps}
+        onChange={this.handleChange}
+        onBlur={this.handleBlur}
+        value={value}
+      />
+    );
+  }
+}
+
+export default FormikDatetime;
