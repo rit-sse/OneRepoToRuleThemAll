ede987cc028d8e6c7067d8db70faaa2679fb1fbe
Bill Dybas <wmdybas@gmail.com>
2018-01-01 19:26:47 -0500

    Add Datetime Component Compatible w/ Formik

--- app/js/index.jsx
+++ app/js/index.jsx
@@ -8,6 +8,7 @@ import { ConnectedRouter } from 'connected-react-router';
 import asyncComponent from 'common/components/AsyncComponent';
 
 import 'scss/app.scss';
+import 'react-datetime/css/react-datetime.css';
 
 import Home from 'home'; // not async
 
