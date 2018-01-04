86ca2ac99caaf805dd85696a4684527a33380830
Bill Dybas <wmdybas@gmail.com>
2018-01-03 23:35:27 -0500

    Begin Officer Form Conversion to Formik
    Warning: Doesn't currently work
    
    Need to figure out a way to use UserForm
    w/ its validations and onSubmit behavior
    in other Formik forms to prevent duplicating
    that validation logic across multiple forms.

--- app/js/officers/containers/OfficerForm.js
+++ app/js/officers/containers/OfficerForm.js
@@ -1,13 +1,11 @@
 import moment from 'moment';
 import { connect } from 'react-redux';
-import { formValueSelector } from 'redux-form';
+
 import { hideModal, OFFICER_MODAL, getCommittees } from 'common/actions';
 import { createOfficer, updateOfficer } from 'officers/actions';
 import OfficerForm from 'officers/components/OfficerForm';
 
-const selector = formValueSelector('officer');
-
-function mapStateToProps({ modal, committees, officers, ...state }) {
+function mapStateToProps({ modal, committees, officers }) {
   const officerObj = officers[modal.id];
   const officer = officerObj ? {
     ...officerObj,
@@ -17,7 +15,6 @@ function mapStateToProps({ modal, committees, officers, ...state }) {
 
   return {
     officer,
-    primaryOfficerValue: selector(state, 'primaryOfficer'),
     isOpen: modal.modalType === OFFICER_MODAL,
     committees: committees.map(committee => ({ label: committee.name, value: committee.name })),
   };
