f819059d5ee1ac3bf9f3e57db98ef79a31bdb248
Bill Dybas <wmdybas@gmail.com>
2018-01-03 23:35:27 -0500

    Begin Officer Form Conversion to Formik
    Warning: Doesn't currently work
    
    Need to figure out a way to use UserForm
    w/ its validations and onSubmit behavior
    in other Formik forms to prevent duplicating
    that validation logic across multiple forms.

--- app/js/officers/components/OfficerForm.jsx
+++ app/js/officers/components/OfficerForm.jsx
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Field, Form, FormSection, reduxForm } from 'redux-form';
-import { Modal, ModalHeader, ModalBody, ModalFooter, InputGroup, InputGroupAddon } from 'reactstrap';
+import { Modal, ModalHeader, ModalBody, ModalFooter, InputGroup, FormFeedback } from 'reactstrap';
+import moment from 'moment';
+import { Formik } from 'formik';
+
+import yup from 'utils/yup';
 import Button from 'common/components/Button';
 import UserForm from 'common/containers/UserForm';
-import SelectInput from 'common/components/SelectInput';
-import moment from 'moment';
+import FormikSelectInput from 'common/components/FormikSelectInput';
 
 class OfficerForm extends Component {
   static propTypes = {
@@ -24,7 +26,6 @@ class OfficerForm extends Component {
         image: PropTypes.string,
       }),
     }),
-    primaryOfficerValue: PropTypes.bool,
     committees: PropTypes.arrayOf(PropTypes.shape({
       label: PropTypes.string,
       value: PropTypes.string,
@@ -33,63 +34,15 @@ class OfficerForm extends Component {
     close: PropTypes.func.isRequired,
     create: PropTypes.func.isRequired,
     update: PropTypes.func.isRequired,
-    handleSubmit: PropTypes.func.isRequired,
-    autofill: PropTypes.func.isRequired,
     getCommittees: PropTypes.func.isRequired,
-    initialize: PropTypes.func.isRequired,
   };
 
   static defaultProps = {
-    officer: null,
-    primaryOfficerValue: false,
+    officer: {},
   };
 
   componentDidMount() {
     this.props.getCommittees();
-  }
-
-  componentDidUpdate(prevProps) {
-    const {
-      isOpen,
-      initialize,
-      officer,
-    } = this.props;
-    if (isOpen && !prevProps.isOpen) {
-      initialize(officer);
-    }
-  }
-
-  submit = (values) => {
-    const {
-      officer,
-      create,
-      update,
-      close,
-    } = this.props;
-
-    const newOfficer = {
-      ...values,
-      startDate: moment(values.startDate).utc(),
-      endDate: moment(values.endDate).utc(),
-      committeeName: do { /* eslint-disable */
-        if (values.primaryOfficer) {
-          'General';
-        } else if (values.committeeName) {
-          values.committeeName.value;
-        } else {
-          undefined;
-        } /* eslint-enable */
-      },
-    };
-    newOfficer.userDce = newOfficer.user.dce;
-
-    close();
-
-    if (officer) {
-      update(officer.id, newOfficer);
-    } else {
-      create(newOfficer);
-    }
   }
 
   render() {
@@ -97,72 +50,230 @@ class OfficerForm extends Component {
       isOpen,
       close,
       officer,
-      handleSubmit,
       committees,
-      autofill,
-      primaryOfficerValue,
+      create,
+      update,
     } = this.props;
 
-    const updateOrCreate = officer ? 'Update' : 'Create';
+    const updateOrCreate = officer.id ? 'Update' : 'Create';
+    const dayFormat = 'YYYY-MM-DD';
+    const primaryOfficerCommitteeSelectOption = { label: 'General', value: 'General' };
 
     return (
       <Modal isOpen={isOpen} toggle={close}>
-        <Form onSubmit={handleSubmit(this.submit)}>
-          <ModalHeader toggle={close}>{updateOrCreate} Officer</ModalHeader>
-          <ModalBody>
-            <FormSection name="user">
-              <UserForm autofill={autofill} />
-            </FormSection>
-            <fieldset className="form-group">
-              <div className="form-group row">
-                <label htmlFor="title" className="col-sm-2 col-form-label">Title</label>
-                <div className="col-sm-10">
-                  <Field id="title" className="form-control" name="title" component="input" />
-                </div>
-              </div>
-              <div className="form-group row">
-                <label htmlFor="email" className="col-sm-2 col-form-label">Alias</label>
-                <InputGroup className="col-sm-10">
-                  <Field id="email" className="form-control" name="email" component="input" />
-                  <InputGroupAddon>@sse.rit.edu</InputGroupAddon>
-                </InputGroup>
-              </div>
-              <div className="form-group row">
-                <label htmlFor="committeeName" className="col-sm-2 col-form-label">Committee</label>
-                <div className="col-sm-10">
-                  <Field id="committeeName" disabled={primaryOfficerValue} input={{ value: primaryOfficerValue ? 'General' : undefined }} name="committeeName" component={SelectInput} options={committees} />
-                </div>
-              </div>
-              <div className="form-check">
-                <label htmlFor="primaryOfficer" className="form-check-label">
-                  <Field id="primaryOfficer" className="form-check-input" name="primaryOfficer" component="input" type="checkbox" />
-                  Primary Officer
-                </label>
-              </div>
-              <div className="form-group row">
-                <label htmlFor="startDate" className="col-sm-2 col-form-label">Start Date</label>
-                <div className="col-sm-10">
-                  <Field id="startDate" className="form-control" name="startDate" component="input" type="date" />
-                </div>
-              </div>
-              <div className="form-group row">
-                <label htmlFor="endDate" className="col-sm-2 col-form-label">End Date</label>
-                <div className="col-sm-10">
-                  <Field id="endDate" className="form-control" name="endDate" component="input" type="date" />
-                </div>
-              </div>
-            </fieldset>
-          </ModalBody>
-          <ModalFooter>
-            <Button className="btn btn-sse" type="submit">{updateOrCreate}</Button>
-            <Button className="btn btn-secondary" type="button" onClick={close}>Cancel</Button>
-          </ModalFooter>
-        </Form>
+        <ModalHeader toggle={close}>{updateOrCreate} Officer</ModalHeader>
+        <Formik
+          initialValues={{
+            title: officer.title || '',
+            email: officer.email || '',
+            committeeName: officer.committeeName // eslint-disable-line no-nested-ternary
+              ? { label: officer.committeeName, value: officer.committeeName }
+              : officer.primaryOfficer ? primaryOfficerCommitteeSelectOption : null,
+            primaryOfficer: !!officer.primaryOfficer,
+            startDate: officer.startDate ? moment(officer.startDate).format(dayFormat) : '',
+            endDate: officer.endDate ? moment(officer.endDate).format(dayFormat) : '',
+          }}
+          validationSchema={() => yup.object()
+            .shape({
+              title: yup.string().required('Required'),
+              email: yup.string().required('Required'),
+              committeeName: yup.object()
+                .shape({
+                  label: yup.mixed(),
+                  value: yup.mixed(),
+                })
+                .required('Required')
+                .nullable(), // react-select uses 'null' to represent when the value is cleared
+              primaryOfficer: yup.boolean(),
+              startDate: yup.date().required('Required'),
+              endDate: yup.date()
+                .when('startDate', {
+                  is: val => !!val,
+                  then: yup.date().isAfter(yup.ref('startDate'), 'End Date must come after Start Date'),
+                }),
+                // TODO: 'endDate' is not required, but since we have to set it's default
+                // value to '' for formik to use it as a controlled component, the 'isAfter'
+                // validation will be active, effectively requiring a value. We need to figure
+                // out how to have yup validate a field only if the field isn't empty (something other than '').
+            })
+          }
+          onSubmit={(
+            values
+          ) => {
+            // TODO: 'values' on has the values defined in 'initialValues'.
+            // We need to figure out a way to get the values from 'UserForm'.
+            const newOfficer = {
+              ...values,
+              startDate: moment(values.startDate).toISOString(),
+              endDate: values.endDate ? moment(values.endDate).toISOString() : null,
+              committeeName: values.committeeName.value, // Parse out the 'select' value
+            };
+            // newOfficer.userDce = newOfficer.user.dce; // TODO: Uncomment when get 'user' value from 'UserForm'
+
+            close();
+
+            if (officer) {
+              update(officer.id, newOfficer);
+            } else {
+              create(newOfficer);
+            }
+          }}
+          render={({
+            values,
+            errors,
+            touched,
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            setFieldValue,
+            setFieldTouched,
+            isSubmitting,
+          }) => (
+            <form onSubmit={handleSubmit}>
+              <ModalBody>
+                <UserForm onSubmit={handleSubmit} user={officer.user} />
+                <fieldset className="form-group">
+                  <div className="form-group row">
+                    <label htmlFor="title" className="col-3 col-form-label">Title</label>
+                    <div className="col-9">
+                      <input
+                        type="text"
+                        name="title"
+                        id="title"
+                        className="form-control"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.title}
+                        required
+                      />
+                      {touched.title
+                        && errors.title
+                        && <FormFeedback style={{ display: 'block' }}>{errors.title}</FormFeedback>}
+                    </div>
+                  </div>
+                  <div className="form-group row">
+                    <label htmlFor="email" className="col-3 col-form-label">Email Alias</label>
+                    <InputGroup className="col-9">
+                      <input
+                        type="text"
+                        name="email"
+                        id="email"
+                        className="form-control"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.email}
+                        required
+                      />
+                      <div className="input-group-append">
+                        <span className="input-group-text">@sse.rit.edu</span>
+                      </div>
+                      {touched.email
+                        && errors.email
+                        && <FormFeedback style={{ display: 'block' }}>{errors.email}</FormFeedback>}
+                    </InputGroup>
+                  </div>
+                  <div className="form-group row">
+                    <label htmlFor="committeeName" className="col-3 col-form-label">Committee</label>
+                    <div className="col-9">
+                      <FormikSelectInput
+                        name="committeeName"
+                        id="committeeName"
+                        field="committeeName"
+                        input={{
+                          onChange: setFieldValue,
+                          onBlur: setFieldTouched,
+                          value: values.committeeName,
+                        }}
+                        options={committees}
+                        disabled={values.primaryOfficer}
+                      />
+                      {touched.committeeName
+                        && errors.committeeName
+                        && <FormFeedback style={{ display: 'block' }}>{errors.committeeName}</FormFeedback>}
+                    </div>
+                  </div>
+                  <div className="form-group">
+                    <div className="form-check">
+                      <label htmlFor="primaryOfficer" className="form-check-label">
+                        <input
+                          type="checkbox"
+                          name="primaryOfficer"
+                          id="primaryOfficer"
+                          className="form-check-input"
+                          onChange={(e) => {
+                            if (e.target.checked) {
+                              // Set to the 'General' committee
+                              setFieldValue('committeeName', primaryOfficerCommitteeSelectOption);
+                            } else {
+                              // Reset the select input
+                              setFieldValue('committeeName', null);
+                            }
+                            handleChange(e);
+                          }}
+                          onBlur={handleBlur}
+                          checked={values.primaryOfficer}
+                        />
+                        Primary Officer
+                      </label>
+                      {touched.primaryOfficer
+                        && errors.primaryOfficer
+                        && <FormFeedback style={{ display: 'block' }}>{errors.primaryOfficer}</FormFeedback>}
+                    </div>
+                  </div>
+                  <div className="form-group row">
+                    <label htmlFor="startDate" className="col-3 col-form-label">Start Date</label>
+                    <div className="col-9">
+                      {/* TODO: Update to use a cross-browser date picker */}
+                      <input
+                        type="date"
+                        name="startDate"
+                        id="startDate"
+                        className="form-control"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.startDate}
+                      />
+                      {touched.startDate
+                        && errors.startDate
+                        && <FormFeedback style={{ display: 'block' }}>{errors.startDate}</FormFeedback>}
+                    </div>
+                  </div>
+                  <div className="form-group row">
+                    <label htmlFor="endDate" className="col-3 col-form-label">End Date</label>
+                    <div className="col-9">
+                      {/* TODO: Update to use a cross-browser date picker */}
+                      <input
+                        type="date"
+                        name="endDate"
+                        id="endDate"
+                        className="form-control"
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        value={values.endDate}
+                      />
+                      {touched.endDate
+                        && errors.endDate
+                        && <FormFeedback style={{ display: 'block' }}>{errors.endDate}</FormFeedback>}
+                    </div>
+                  </div>
+                </fieldset>
+              </ModalBody>
+              <ModalFooter>
+                <Button
+                  type="submit"
+                  className="btn btn-sse"
+                  disabled={isSubmitting}
+                >
+                  {updateOrCreate}
+                </Button>
+                <Button className="btn btn-secondary" type="button" onClick={close}>Cancel</Button>
+              </ModalFooter>
+            </form>
+          )}
+        />
       </Modal>
     );
   }
 }
 
-export default reduxForm({
-  form: 'officer',
-})(OfficerForm);
+export default OfficerForm;
