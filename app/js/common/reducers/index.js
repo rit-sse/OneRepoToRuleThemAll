c01b96ac035757e148932f049ce470a49a4e6aca
Bill Dybas <wmdybas@gmail.com>
2018-01-15 12:02:13 -0500

    Remove Mentoring
    Closes #163
    Closes #27
    Closes #10

--- app/js/common/reducers/index.js
+++ app/js/common/reducers/index.js
@@ -1,6 +1,5 @@
 import auth from './auth';
 import committees from './committees';
-import hover from './hover';
 import modal from './modal';
 import nav from './nav';
 import scroll from './scroll';
@@ -10,7 +9,6 @@ import user from './user';
 export default {
   auth,
   committees,
-  hover,
   modal,
   nav,
   scroll,
--- app/js/mentoring/actions/index.js
+++
@@ -1,3 +0,0 @@
-export * from './shifts';
-export * from './mentors';
-export * from './specialties';
