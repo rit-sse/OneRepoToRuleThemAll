37345b1f72cd7817f6bc1bb72d64222c75cb60e3
Robert McLaughlin <robert@sparkk.us>
2018-02-14 15:46:28 -0500

    hide officer memberships under a secondary tab

--- app/js/scoreboard/containers/MembersList.js
+++ app/js/scoreboard/containers/MembersList.js
@@ -3,12 +3,15 @@ import { viewMembershipModal } from 'common/actions';
 import List from 'common/components/List';
 import Member from 'scoreboard/components/Member';
 import { getMembers } from 'scoreboard/actions';
+import { getOfficers } from 'officers/actions';
 
-function mapStateToProps(store) {
+function mapStateToProps(store, { displayedType }) {
+  const officerDces = Object.values(store.officers).map(officer => officer.userDce);
   return {
     item: Member,
     items: Object
       .keys(store.members)
+      .filter(dce => (displayedType === 'MEMBERS' ? !officerDces.includes(dce) : officerDces.includes(dce)))
       .sort((a, b) => store.members[b].count - store.members[a].count)
       .map((el, index) => ({
         ...store.members[el],
@@ -20,7 +23,10 @@ function mapStateToProps(store) {
 
 function mapDispatchToProps(dispatch) {
   return {
-    getItems: () => dispatch(getMembers()),
+    getItems: () => {
+      dispatch(getMembers());
+      dispatch(getOfficers());
+    },
     viewItem: id => dispatch(viewMembershipModal(id)),
   };
 }
--- app/js/scoreboard/index.js
+++ app/js/scoreboard/index.js
@@ -1,9 +1,11 @@
 import { injectAsyncReducer } from 'store';
+import officers from 'officers/reducers';
 
 import Page from './Page';
 import { memberships, members } from './reducers';
 
 injectAsyncReducer('members', members);
 injectAsyncReducer('memberships', memberships);
+injectAsyncReducer('officers', officers);
 
 export default Page;
