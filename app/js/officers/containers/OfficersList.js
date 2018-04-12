34fd35eae257df13a65447716d68c1959aaeb504
Wade Mauger <wadeanthony0100@gmail.com>
2018-04-12 11:57:09 -0400

    Add officer sorting to OfficerList

--- app/js/officers/containers/OfficersList.js
+++ app/js/officers/containers/OfficersList.js
@@ -4,10 +4,27 @@ import List from 'common/components/List';
 import { getOfficers } from 'officers/actions';
 import Officer from 'officers/components/Officer';
 
+const order = new Map([
+  ['President', 0],
+  ['Vice President', 1],
+  ['Treasurer', 2],
+  ['Secretary', 3],
+  ['Technology Head', 4], // dev only
+]);
+
+const sortPredicate = (officerA, officerB) => {
+  if (order.get(officerA.title) > order.get(officerB.title)) {
+    return 1;
+  }
+  return -1;
+}
+
 function mapStateToProps({ auth, officers }, { primary = false }) {
   return {
     item: Officer,
-    items: Object.values(officers).filter(({ primaryOfficer }) => primaryOfficer === primary),
+    items: Object.values(officers)
+      .filter(({ primaryOfficer }) => primaryOfficer === primary)
+      .sort(sortPredicate),
     itemProps: {
       showActions: !!(auth.officer && auth.officer.primaryOfficer),
     },
