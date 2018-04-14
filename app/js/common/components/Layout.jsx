f44e4c04761aa0915c3923e11b31d85d74af162d
Alex Taffe <alex.taffe@gmail.com>
2018-04-14 01:56:49 -0400

    Copyright Updates (#198)
    * Update the website copyright
    * Update LICENSE.md to have the correct year and organization name
    * Update projects.md - Merge project Darby and project pi
    * Output the copyright year dynamically so it does not have to be updated in the future

--- app/js/common/components/Layout.jsx
+++ app/js/common/components/Layout.jsx
@@ -4,11 +4,14 @@ import SSENav from 'common/containers/SSENav';
 
 import 'scss/footer.scss';
 
+// get the current year so we can output the copyright notice
+const copyrightDate = new Date().getFullYear();
+
 const Layout = props => (
   <div>
     <SSENav />
     {props.children}
-    <footer className="sse-footer">All content is Copyright © 2017 Society of Software Engineers.</footer>
+    <footer className="sse-footer">All content is Copyright © {copyrightDate} Society of Software Engineers.</footer>
   </div>
 );
 
