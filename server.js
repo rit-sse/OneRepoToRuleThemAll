363c393b5de2074d0f0bbe24bd9c1ec4b5fab9b3
Alex Atwater <akath20@gmail.com>
2018-09-25 14:47:11 -0400

    Fixed output host to 0.0.0.0

--- server.js
+++ server.js
@@ -14,5 +14,5 @@ server.listen(process.env.PORT || 5000, '0.0.0.0', function (err) {
   if (err) {
     console.log(err);
   }
-  console.log('Listening at localhost:' + (process.env.PORT || 5000));
+  console.log('Listening at 0.0.0.0:' + (process.env.PORT || 5000));
 });
