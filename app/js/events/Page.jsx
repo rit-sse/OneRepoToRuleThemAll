9e3eddc205a2d95307245f714286869599e769f3
Alex Atwater <akath20@gmail.com>
2018-09-28 13:11:41 -0400

    Add calendar icon to download calendar events.

--- app/js/events/Page.jsx
+++ app/js/events/Page.jsx
@@ -4,6 +4,7 @@ import CreateButton from 'common/containers/CreateButton';
 import { showEventModal, COMMITTEES } from 'common/actions';
 import Status from 'common/containers/Status';
 import Login from 'common/containers/Login';
+import Button from 'common/components/Button';
 import CommitteesList from 'events/containers/CommitteesList';
 import EventModal from 'events/containers/EventModal';
 import EventList from 'events/containers/EventList';
@@ -17,12 +18,18 @@ const Event = () => (
         <Link to="/events" className="title-link">
           <h2 className="pull-left">Events</h2>
         </Link>
+
         <div className="btn-group pb-2 mt-sm-1 float-right" role="group" aria-label="Basic example">
           <CreateButton action={showEventModal} className="btn btn-secondary" >
             Create
           </CreateButton>
           <Login className="btn btn-sse" />
         </div>
+        <Link to="/api/v2/events.ics" className="float-right" style={{marginTop: "4px", marginRight: "4px"}}>
+            <Button className="btn btn-sse">
+                <i className="fa fa-calendar" aria-hidden="true" alt="Import Calendar" title="Import Calendar" />
+            </Button>
+        </Link>
       </div>
     </div>
     <div className="row">
