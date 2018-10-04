f2ac8c19b0a3da84362c7e31345553a0fe1189ef
Seppe Snoeck <seppe.snoeck@student.odisee.be>
2018-10-04 21:11:34 +0200

    Added an awesome 404 not found page

--- app/js/static/Page.jsx
+++ app/js/static/Page.jsx
@@ -22,7 +22,21 @@ const Static = ({ location }) => {
   }
   return (
     <div className="container page">
-      <h4>Page Not Found</h4>
+      <div id="notfound">
+        <div className="notfound">
+          <div className="notfound-404">
+            <h1>
+              4<span />4
+            </h1>
+          </div>
+          <h2>Oops! Page Not Be Found</h2>
+          <p>
+            Sorry but the page you are looking for does not exist, have been
+            removed. name changed or is temporarily unavailable
+          </p>
+          <a href="/">Back to homepage</a>
+        </div>
+      </div>
     </div>
   );
 };
