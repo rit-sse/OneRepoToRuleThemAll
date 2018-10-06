01ca77fd2b9e967b1420b653af0d1ae51aed1ae8
Bill Dybas <wmdybas@gmail.com>
2018-10-06 06:46:54 -0700

    Convert Not Found Sass to Styled Components

--- app/js/static/Page.jsx
+++ app/js/static/Page.jsx
@@ -1,8 +1,10 @@
 /* eslint-disable react/no-danger */
 import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+
 import 'scss/page.scss';
+
+import NotFound from './NotFound';
 
 function getPage(page) {
   try {
@@ -23,20 +25,7 @@ const Static = ({ location }) => {
   }
   return (
     <div className="container page">
-      <div id="notfound">
-        <div className="notfound">
-          <div className="notfound-404">
-            <h1>
-              4<span />4
-            </h1>
-          </div>
-          <h2>Oops! Page Not Found</h2>
-          <p>
-            Sorry! The page you were trying to reach does not exist or no longer exists.
-          </p>
-          <Link to="/">Back to homepage</Link>
-        </div>
-      </div>
+      <NotFound />
     </div>
   );
 };
