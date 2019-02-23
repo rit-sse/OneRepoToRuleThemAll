6db73f5082939cfe506feb9184c394287fc2ed45
Scott <saf6260@rit.edu>
2019-02-23 18:15:38 -0500

    Updating go links. Need to finish styling an element but other than that public options are now working properly

--- app/js/go/containers/GoLinksList.js
+++ app/js/go/containers/GoLinksList.js
@@ -3,16 +3,23 @@ import { showGoModal } from 'common/actions';
 import List from 'common/components/List';
 import { getLinks, destoryLink } from 'go/actions';
 import GoLink from 'go/components/GoLink';
+import PublicGoLink from 'go/components/PublicGoLink';
 
 function mapStateToProps({ go, auth, scroll }) {
   return {
     scroll: true,
     scrollDone: scroll || !auth.officer,
-    item: GoLink,
+    item: auth.officer ? GoLink : PublicGoLink,
     wrapper: 'ul',
     wrapperProps: { className: 'list-group' },
-    items: auth.officer ? go.all : go.allPublic,
+    items: go.all.filter((link) => {
+      if (auth.officer !== null) {
+        return true;
+      }
+      return link.public;
+    }),
     keyPriority: ['shortLink'],
+    forceGetItems: true,
   };
 }
 
