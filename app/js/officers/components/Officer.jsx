d85d0214128e1d158fc4cb112958d76b185905f5
Bill Dybas <wmdybas@gmail.com>
2019-10-19 15:42:24 -0700

    Make Officer Image Linkable

--- app/js/officers/components/Officer.jsx
+++ app/js/officers/components/Officer.jsx
@@ -8,6 +8,7 @@ class Officer extends Component {
   static propTypes = {
     title: PropTypes.string.isRequired,
     email: PropTypes.string.isRequired,
+    linkedinUrl: PropTypes.string,
     user: PropTypes.shape({
       firstName: PropTypes.string,
       lastname: PropTypes.string,
@@ -44,6 +45,7 @@ class Officer extends Component {
       title,
       user,
       email,
+      linkedinUrl,
       editItem,
       deleteItem,
       showActions,
@@ -54,7 +56,13 @@ class Officer extends Component {
     return (
       <SwipeArea onLeft={this.showActions} onRight={this.hideActions} className="actions-container-fade col-sm-6 col-lg-3 pb-3">
         <div className="media">
-          <img className="mr-3" src={gravatar(dce, image)} alt="Officer" height={80} />
+          {linkedinUrl ? (
+            <a href={linkedinUrl} target="_blank" rel="noopener noreferrer">
+              <img className="mr-3" src={gravatar(dce, image)} alt="Officer" height={80} />
+            </a>
+          ) : (
+            <img className="mr-3" src={gravatar(dce, image)} alt="Officer" height={80} />
+          )}
           <div className="d-flex align-self-center flex-column media-body">
             <h5 className="mb-0">{title}</h5>
             <p className="m-0">
