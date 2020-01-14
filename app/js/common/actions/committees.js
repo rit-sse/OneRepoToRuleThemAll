fc9a9a73e85322100528c244105d04eb42e77ce2
Gavriel R-H <gavriel@gavrielrh.com>
2020-01-13 22:37:58 -0500

    Resolve bug where including a new Date() object as part of the committee filter param was resulting in duplicate committies

--- app/js/common/actions/committees.js
+++ app/js/common/actions/committees.js
@@ -9,7 +9,7 @@ const loading = utils.createLoading(COMMITTEES);
 export function getCommittees() {
   return (dispatch, getState, { Committees }) => {
     dispatch(loading(GET_COMMITTEES));
-    Committees.all({ active: new Date() }, true)
+    Committees.all({}, true)
       .then(data => dispatch(createAction(GET_COMMITTEES, data)))
       .catch(err => dispatch(createAction(GET_COMMITTEES, err)));
   };
