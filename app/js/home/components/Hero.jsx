f21e8aef80bf177f595053df486c1f2ed1331c33
meg5228 <meg5228@g.rit.edu>
2021-02-10 11:36:14 -0500

    Updates to main page's text to make it accurate for the current semester

--- app/js/home/components/Hero.jsx
+++ app/js/home/components/Hero.jsx
@@ -33,7 +33,7 @@ class Hero extends React.Component {
           <div className="fancy-hero-container" />
           <div className="hero-content">
             <h3>Weekly Meetings</h3>
-            <h6>Thursdays @ 4:00pm</h6>
+            <h6>Mondays @ 4:30pm</h6>
             <h6><a href="https://sse.rit.edu/go/generalmeeting">Click here to join the Zoom</a></h6>
             <h6>All Are Welcome!</h6>
           </div>
